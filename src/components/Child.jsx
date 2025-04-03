e2acffb59a76e6c02fc63af4df0e38928599b75e
18subrato <subratoverma7@gmail.com>
2025-04-03 17:15:10 +0530

    liftup5

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Child = ({ isLoggedIn, setIsLoggedIn }) => {
     return (
         <div>
-            {isLoggedIn ? 'You are logged in!' : <div>Username:<input
+            {isLoggedIn ? 'You are logged in!' : <form>Username:<input
                 type='text'
             />
                 Password:<input
                     type='password'
                 />
                 <button onClick={() => setIsLoggedIn(true)}>Login</button>
-                </div>}
+                </form>}
     </div>
     )
 }
